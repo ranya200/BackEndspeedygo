e2bdf5a41c1ac140b8bdd4a47b7f11f7eb1ca39d
Youssef <youssef.rahali@esprit.tn>
2025-03-02 14:48:34 +0100

    keycloak backend fixed , id user no

--- src/main/java/tn/esprit/examen/SpeedyGo/entities/User.java
+++ src/main/java/tn/esprit/examen/SpeedyGo/entities/User.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "users") // Defines the MongoDB collection
 @Getter
@@ -20,5 +22,5 @@ public class User {
     private String email;
     private String firstName;
     private String lastName;
-    private String role;
+    private List<String> roles;
 }
