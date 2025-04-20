bd51518e9468bc8500a68df88226bc20de91a373
Youssef <youssef.rahali@esprit.tn>
2025-04-20 16:51:45 +0100

    Admin Produit validation fixed

--- src/main/java/tn/esprit/examen/SpeedyGo/Services/OtpService.java
+++ src/main/java/tn/esprit/examen/SpeedyGo/Services/OtpService.java
@@ -3,6 +3,7 @@ package tn.esprit.examen.SpeedyGo.Services;
 import com.twilio.Twilio;
 import com.twilio.rest.verify.v2.service.Verification;
 import com.twilio.rest.verify.v2.service.VerificationCheck;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value; // ✅ Correct
 import org.springframework.stereotype.Service;
@@ -21,8 +22,14 @@ public class OtpService {
 
     @PostConstruct
     public void init() {
+        Dotenv dotenv = Dotenv.load();
+        accountSid = dotenv.get("TWILIO_ACCOUNT_SID");
+        authToken = dotenv.get("TWILIO_AUTH_TOKEN");
+        serviceSid = dotenv.get("TWILIO_SERVICE_SID");
+
         Twilio.init(accountSid, authToken);
     }
+
 
     public void sendOtp(String phoneNumber) {
         Verification.creator(serviceSid, phoneNumber, "sms").create();
