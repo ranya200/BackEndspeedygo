5dbc5908f6dc0a86c9f6111dae0933ed590050eb
Youssef <youssef.rahali@esprit.tn>
2025-05-04 11:59:41 +0100

    ⏪ Revenir à l’état initial du projet (commit b398a1e...)

--- src/main/java/tn/esprit/examen/SpeedyGo/SpeedyGo.java
+++ src/main/java/tn/esprit/examen/SpeedyGo/SpeedyGo.java
@@ -4,27 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 
 @EnableAspectJAutoProxy
 @EnableScheduling
-
-@OpenAPIDefinition(
-        info = @Info(
-                title = "SpeedyGo API",
-                version = "1.0",
-                description = "Navigators Team API"
-        ),
-        servers = {
-                @Server(url = "http://localhost:8089/speedygo", description = "Local Development Server")
-        }
-)
-@SpringBootApplication(scanBasePackages = "tn.esprit.examen.SpeedyGo") // ✅ Vérifie bien ce package
-
+@SpringBootApplication
 public class SpeedyGo {
+
     public static void main(String[] args) {
         SpringApplication.run(SpeedyGo.class, args);
     }
+
 }
